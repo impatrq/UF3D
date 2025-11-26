ed705f2e7c7e4ea0246f171f7af96f7f4113c852
valentin <vzaccari33@gmail.com>
2025-11-25 23:25:10 -0300

    Update MotoresTesetoSimultaneo.py

--- software/motores/MotoresTesetoSimultaneo.py
+++ software/motores/MotoresTesetoSimultaneo.py
@@ -26,3 +26,12 @@ def take_step_simultaneous():
     step_pin_2.value(0)
     step_pin_3.value(0)
     time.sleep(STEP_DELAY)
+
+def main_test():
+    set_directions(1, 0, 1)
+    
+    sys.stdout.write("Test de Motores Triples Iniciado. Presiona Ctrl+C para detener.\n")
+
+    try:
+        while True:
+            take_step_simultaneous()
