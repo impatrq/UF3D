daae6736ff6a067c5ead80d7369aae39c95cc8c6
valentin <vzaccari33@gmail.com>
2025-11-25 23:39:14 -0300

    Update lidarconmotor.py

--- software/Lidar/lidarconmotor.py
+++ software/Lidar/lidarconmotor.py
@@ -2,7 +2,7 @@ from machine import Pin, UART
 import utime as time 
 import sys 
 
-# --- CONFIGURACIÓN DEL LIDAR TF-LC02 --- 
+
 UART_TX_PIN = 0 
 UART_RX_PIN = 1 
 LIDAR_BAUDRATE = 115200 
@@ -13,9 +13,16 @@ RESPONSE_LEN = 8
 uart = UART(0, baudrate=LIDAR_BAUDRATE, tx=Pin(UART_TX_PIN), rx=Pin(UART_RX_PIN)) 
 
 
-
+ 
 DIR_PIN = 17 
 STEP_PIN = 18 
 STEPS_PER_REV = 200 
 STEP_DELAY = 0.001 
+
+ 
+STEPS_PER_SAMPLE = 10  
+TOTAL_SAMPLES = int(STEPS_PER_REV / STEPS_PER_SAMPLE) 
+
+dir_pin = Pin(DIR_PIN, Pin.OUT) 
+step_pin = Pin(STEP_PIN, Pin.OUT) 
 
