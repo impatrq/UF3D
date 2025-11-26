8438a2fb12fb4209c7581d46daa55f71d1c965ca
valentin <vzaccari33@gmail.com>
2025-11-25 23:36:35 -0300

    Update lidarconmotor.py

--- software/Lidar/lidarconmotor.py
+++ software/Lidar/lidarconmotor.py
@@ -2,3 +2,10 @@ from machine import Pin, UART
 import utime as time 
 import sys 
 
+
+UART_TX_PIN = 0 
+UART_RX_PIN = 1 
+LIDAR_BAUDRATE = 115200 
+LIDAR_STABILIZATION_MS = 500 
+CMD_MEASURE_DISTANCE = b'\x55\xAA\x81\x00\xFA' 
+RESPONSE_LEN = 8 
