48e5772db52220a56a55b262c32207895a960ece
valentin <vzaccari33@gmail.com>
2025-11-25 21:01:36 -0300

    Update MotoresMixto.py

--- software/motores/MotoresMixto.py
+++ software/motores/MotoresMixto.py
@@ -3,7 +3,7 @@ import time
 
 pinStep_M1 = 12
 pinStep_M2 = 13
-pinDirection = 17
+pinDirection = 17 
 
 stepsPerRevolution = 200
 stepDelay = 0.005
@@ -17,25 +17,23 @@ GPIO.setup(pinDirection, GPIO.OUT)
 if __name__ == '__main__':
     try:
         GPIO.output(pinDirection, GPIO.HIGH)
-
-
-while True:
+        
+        while True:
             print("Iniciando una vuelta completa (200 pasos)...")
-
-
-for step_count in range(stepsPerRevolution):
+            
+            for step_count in range(stepsPerRevolution):
                 GPIO.output(pinStep_M1, GPIO.HIGH)
                 GPIO.output(pinStep_M2, GPIO.HIGH)
                 time.sleep(stepDelay)
                 
- GPIO.output(pinStep_M1, GPIO.LOW)
+                GPIO.output(pinStep_M1, GPIO.LOW)
                 GPIO.output(pinStep_M2, GPIO.LOW)
                 time.sleep(stepDelay) 
-
-print("Vuelta completada. Pausa de 1 segundo.")
+                
+            print("Vuelta completada. Pausa de 1 segundo.")
             time.sleep(1)
 
-except KeyboardInterrupt:
+    except KeyboardInterrupt:
         pass 
 
     finally:
